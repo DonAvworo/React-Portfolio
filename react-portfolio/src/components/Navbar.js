40f1f863d3a815a9c56ce9b723142b8b7cd9524b
Don Avworo <theprodigaldon@icloud0.com>
2022-09-08 15:28:18 +0100

    navbar links functional

--- react-portfolio/src/components/Navbar.js
+++ react-portfolio/src/components/Navbar.js
@@ -1,1 +1,32 @@
-import 
+import './Navbar.css';                              // Import css file
+import { Link } from 'react-router-dom';            // Import Link from react-router-dom
+import React from 'react';                          // imports the react library from the node_modules folder and assigns it to the variable React
+
+const Navbar = () => {
+  return (
+    <div>
+        <Link to="/">                               {/* the link to the Home.js (Home Page route path is "/")  */}                
+            <div className="logo">Don-Avworo</div>  {/* the logo of the website  when clicked redirects to Home.js (Home Page)*/}
+        </Link>                                     {/* to work, this will be called as a component in te Home.js file */}
+        <ul className="nav-links">                  {/* unordered list of links to the other pages of the website */}
+            <Link to="/">                           {/* the link to the Home.js (Home Page route path is "/")  */}
+                <li>Home</li>                       {/* the link to the Home.js (Home Page)  */}
+            </Link>
+            <Link to="/about">                      {/* the link to the About.js (About Page route path is "/about")  */}
+                <li>About</li>                      {/* the link to the About.js (About Page)  */}
+            </Link>
+            <Link to="/projects">                   {/* the link to the Projects.js (Projects Page route path is "/projects")  */}
+                <li>Projects</li>                   {/* the link to the Projects.js (Projects Page)  */}
+            </Link>
+            <Link to="/contact">                    {/* the link to the Contact.js (Contact Page route path is "/contact")  */}
+                <li>Contact</li>                    {/* the link to the Contact.js (Contact Page)  */}
+            </Link>
+        </ul>
+
+
+    </div>
+
+  )
+}
+
+export default Navbar;
--- react-portfolio/src/routes/Home.js
+++ react-portfolio/src/routes/Home.js
@@ -1,11 +1,15 @@
  // functional component that returns the following HTML to the browser when the Home function component is called
- 
-import React from 'react'   // imports the react library from the node_modules folder and assigns it to the variable React
+import React from 'react';   // imports the react library from the node_modules folder and assigns it to the variable React
+import Navbar from '../components/Navbar'; // imports the Navbar component from the Navbar.js file in the components folder and assigns it to the variable Navbar
 
 const Home = () => {        // creates a function component called Home
   return (                  // returns the following HTML to the browser when the Home function component is called
-    <div>Home</div>
+    <div>
+      {/* Home */}         {/* Home is commented out and replaced with */}
+      <Navbar />           {/* Navbar component */}
+    </div>
+    
   )
 }
 
-export default Home        // exports the Home function component to be used in other files
+export default Home;        // exports the Home function component to be used in other files
