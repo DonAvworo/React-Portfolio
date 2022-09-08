a5192f7f7e519478a6155d73d50de8b7214e93b0
Don Avworo <theprodigaldon@icloud0.com>
2022-09-08 10:53:37 +0100

    installed other depencies 🍒

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";  // imports the react library from the node_modules folder and assigns it to the variable React
+import "./index.css";       // imports the index.css file from the same folder as the App.js file to be used in the App function component
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // The following code is the HTML that will be rendered to the browser when the App function component is called
+    <div>
+      <h1>Hello World!</h1>
     </div>
   );
 }
