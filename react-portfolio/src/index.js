48918772521a041e62250805b42f33a581c0b595
Don Avworo <theprodigaldon@icloud0.com>
2022-09-08 10:54:40 +0100

    additional packages installed 🔥

--- react-portfolio/src/index.js
+++ react-portfolio/src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react';                // imports the react library from the node_modules folder and assigns it to the variable React
+import ReactDOM from 'react-dom/client';  // imports the react-dom library from the node_modules folder and assigns it to the variable ReactDOM
+import './index.css';                     // imports the index.css file from the same folder as the index.js file to be used in the App function component
+import App from './App';                  // imports the App function component from the App.js file in the same folder
+import { BrowserRouter } from 'react-router-dom';  // imports the BrowserRouter component from the react-router-dom library from the node_modules folder
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(                          // renders the App function component to the browser
+  // the following code is the HTML that will be rendered to the browser when the App function component is called
+
+  // the BrowserRouter component is used to wrap the App function component...
+  //...so that the App function component can use the react-router-dom library
+  <BrowserRouter>                         
     <App />
-  </React.StrictMode>
+  </BrowserRouter>,
+  document.getElementById('root')         // renders the App function component to the element with the id of root in the index.html file in the public folder
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
