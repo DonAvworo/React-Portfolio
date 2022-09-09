4dc20d1a6ddc220005e957630a3fed6667dfd869
Don Avworo <theprodigaldon@icloud0.com>
2022-09-09 12:58:44 +0100

    completed hero with 🖱 buttons

--- react-portfolio/src/components/Hero.js
+++ react-portfolio/src/components/Hero.js
@@ -1,6 +1,7 @@
 import "./Hero.css";                // Importing the CSS file
 import React from 'react';           // imports the react library from the node_modules folder and assigns it to the variable React
-import HeroImg from "../assets/img/HeroImg.png"; // imports the HeroImg.png image from the src folder and assigns it to the variable HeroImg
+import HeroImg from "../assets/img/HeroImg-2.png"; // imports the HeroImg.png image from the src folder and assigns it to the variable HeroImg
+import { Link } from 'react-router-dom'; // imports the Link component from the react-router-dom library and assigns it to the variable Link
 
 const Hero = () => {
   return (
@@ -11,12 +12,13 @@ const Hero = () => {
         </div>
         
         <div className="hero-text-contents">
-            <h1>Hi, I'm <span className="hero-name">Don Avworo</span></h1>
+            <h1>Hi, I'm </h1>
+            <h1> <span className="hero-name">Don Avworo</span></h1>
             <p>I'm a Full Stack Web Developer</p>
 
             <div className="hero-btns">
-                <a href="#projects" className="btn btn-primary">My Projects</a>
-                <a href="#contact" className="btn btn-secondary">Contact Me</a>
+                <Link to="/Projects" className="btn btn-primary">My Projects</Link>
+                <Link to="/Contact" className="btn btn-secondary">Contact Me</Link>
             </div>
         </div> 
     </div>
