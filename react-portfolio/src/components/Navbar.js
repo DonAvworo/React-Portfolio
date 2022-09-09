19fe93a3dc6cae7ad09f09409f46f39776a8599c
Don Avworo <theprodigaldon@icloud0.com>
2022-09-09 09:16:46 +0100

    Navbar component completed

--- react-portfolio/src/components/Navbar.js
+++ react-portfolio/src/components/Navbar.js
@@ -1,15 +1,23 @@
 import './Navbar.css';                              // Import css file
 import { Link } from 'react-router-dom';            // Import Link from react-router-dom
 import React from 'react';                          // imports the react library from the node_modules folder and assigns it to the variable React
+import { FaBars } from 'react-icons/fa';            // imports the FaBars component from the react-icons library from the node_modules folder and assigns it to the variable FaBars
+import { FaTimes } from 'react-icons/fa';           // imports the FaTimes component from the react-icons library from the node_modules folder and assigns it to the variable FaTimes
 
 const Navbar = () => {
+
+    // javascript code that will be executed when the Navbar function component (harmbuger and close icons) is called 
+    const [click, setClick] = React.useState(false);  // creates a state variable called click and assigns it the value of false
+    const handleClick = () => setClick(!click);       // creates a function called handleClick that sets the value of click to the opposite of its current value (see NOTE-2 below)
+    
+
   return (
     <div className='header'>
         <Link to="/">                               {/* the link to the Home.js (Home Page route path is "/")  */}                
             <h1 className="name-logo">Don-Avworo</h1>  {/* the logo of the website  when clicked redirects to Home.js (Home Page)*/}
         </Link>                                     {/* to work, this will be called as a component in te Home.js file */}
-        <ul className="nav-links">                  {/* unordered list of links to the other pages of the website */}
-            <li>
+        <ul className={click ? "nav-links active" : "nav-links"}>  
+            <li>                
                 <Link to="/">Home</Link>             {/* the link to the Home.js (Home Page route path is "/")  */}
             </li>
             <li>
@@ -22,6 +30,13 @@ const Navbar = () => {
                 <Link to="/contact">Contact</Link>   {/* the link to the Contact.js (Contact Page route path is "/contact")  */}
             </li>
         </ul>
+
+
+         {/*hambuger and close icons  */}
+        <div className="hambuger-menu" onClick={handleClick}>
+            {click ? (<FaTimes style={{color: "white"}} size={30}/>) : (<FaBars style={{color: "white"}} size={30}/>)}  
+        </div>
+      
     </div>
 
   )
@@ -36,3 +51,10 @@ export default Navbar;
 // the to property is the same as the href property in HTML
 // the Link property or component is imported from the react-router-dom library
 // the Link component is used to link to other pages of the website
+
+
+// NOTES: -2  import { useState } from 'react';  This imports the useState function from the react library from the node_modules folder and assigns it to the variable useState
+// here userState is imported to be used to toggle the hamburger and close icons
+// this method can be used along with the click, setClick method to toggle the icons LIKE THIS:
+// ...const [click, setClick] = useState(false); This can substitute the code in line 10 above (useState is imported from react and called)
+// in line 10 above, React.useState is called to use the useState function from the react library
