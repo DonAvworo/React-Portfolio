ee3fb849ae6a35785a7e02da0fbbaa3faf4f1b50
Don Avworo <theprodigaldon@icloud0.com>
2022-09-12 23:18:40 +0100

    contact info added

--- react-portfolio/src/components/Hero3.js
+++ react-portfolio/src/components/Hero3.js
@@ -1,7 +1,7 @@
 import "./Hero3.css";
 import Hero3Img from "../assets/img/Contact.png";
 import React from "react";
-import { FaMailBulk, FaPhone } from "react-icons/fa";
+import { FaPhone } from "react-icons/fa";
 
 const Hero3 = () => {
   return (
@@ -12,9 +12,14 @@ const Hero3 = () => {
 
       <div className="hero2-text-contents">
         <FaPhone></FaPhone>
-        <h1 className="hero3-title">07505729649</h1> 
-        <FaMailBulk></FaMailBulk>
-        <p className="hero3-subtitle">theprodigaldon@icloud.com</p>
+       {/* CONTACT FORM */}
+        <h1 className="hero2-text">Contact Me</h1>
+        <p className="hero2-text">I am currently available to work in any project. If you have any questions or would like to work together, please feel free to contact me.</p>
+        <div className="hero2-btn">
+          <a href="mailto:" className="btn">Email</a>
+          <a href="tel:" className="btn">Phone</a>
+        </div>
+
       </div>
     </div>
   )
